1e146f079b0129d31cbc39bf11174bc93e21f216
JijaEdem666 <egorvmamontov@gmail.com>
2024-03-16 12:53:53 +0400

    fourth task

--- pract4/four.py
+++ pract4/four.py
@@ -12,11 +12,11 @@ root.geometry("300x150")
 root.title("Задание 4")
 root.config(bg="orange")
 
-entry = Entry(width=50, background="DeepSkyBlue", foreground="white", borderwidth=5, relief="solid")
+entry = Entry(width=50, background="DeepSkyBlue", foreground="white", borderwidth=5, relief="solid", font=("Arial", 12))
 entry.insert(0 ,"Введите ваше имя:")
 entry.pack()
 
-bt = Button(text="Нажмите",command=create_label, bg="white", fg="DodgerBlue", borderwidth=1, relief="solid")
+bt = Button(text="Нажмите",command=create_label, bg="white", fg="DodgerBlue", borderwidth=1, relief="solid", font=("Arial", 11))
 bt.pack()
 
 root.mainloop()
