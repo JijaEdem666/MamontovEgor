3246485c1ba3a78b55a0788919adde87848f7ee4
JijaEdem666 <egorvmamontov@gmail.com>
2024-03-16 12:45:57 +0400

    fourth task

--- pract4/four.py
+++ pract4/four.py
@@ -1,10 +1,22 @@
 from tkinter import *
 
 
+def create_label():
+    s = entry.get()
+    label = Label(text=f"Привет {s}")
+    label.pack()
+
+
 root = Tk()
 root.geometry("300x150")
 root.title("Задание 4")
 root.config(bg="orange")
 
+entry = Entry(width=50, background="DeepSkyBlue", foreground="white", borderwidth=5, relief="solid")
+entry.insert(0 ,"Введите ваше имя:")
+entry.pack()
+
+bt = Button(text="Нажмите",command=create_label, bg="white", fg="DodgerBlue", borderwidth=1, relief="solid")
+bt.pack()
 
 root.mainloop()
