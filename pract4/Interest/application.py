2fabe1ec298e3ae54ede8c83c37a127551114129
JijaEdem666 <egorvmamontov@gmail.com>
2024-03-30 12:50:42 +0400

    seventh task

--- pract4/Interest/application.py
+++ pract4/Interest/application.py
@@ -1,9 +1,12 @@
 from mainWindow import MainWindow
 from tkinter import *
+import pathlib
 
 root = Tk()
 root.geometry("500x500")
 root.title("Interest")
+file = pathlib.Path().joinpath("interest.ico").resolve()
+root.iconbitmap(file)
 
 
 mw = MainWindow(root)
