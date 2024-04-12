06e89c2ec34fcaa31d1a115eb5a26bb7a55702d4
JijaEdem666 <egorvmamontov@gmail.com>
2024-04-12 17:25:30 +0400

    2_3_1 task

---
+++ pract2/2_3_1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+plt.figure(figsize=(6, 6))
+plt.axes(projection='polar')
+
+r = 2 * np.random.random(150)
+theta = 2 * np.pi * np.random.random(150)
+
+plt.scatter(theta, r, s=400*r**2, c=theta, cmap='hsv', alpha=0.8)
+plt.show()
